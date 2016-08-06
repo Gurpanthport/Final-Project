26827135dd32c7739a917e5db2bd481ae860d957
gurpanthport <gurpanth1996@hotmail.com>
2016-08-05 21:49:31 -0400

    Changes

---
+++ final-project/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Electronics Store.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact US.";
+
+            return View();
+        }
+    }
+}
